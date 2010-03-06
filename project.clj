876c56036fbd4172b7d61ca532270631adac9720
Peter Schuller <peter.schuller@infidyne.com>
2010-03-06 23:28:18 +0100

    Add basic cmdline parsing.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject sob "0.0.1"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [rhino/js "1.7R2"]]
+                 [rhino/js "1.7R2"]
+                 [compojure "0.3.2"]]
   :main org.scode.sob)
--- src/org/scode/sob.clj
+++ src/org/scode/sob.clj
@@ -1,6 +1,29 @@
 (ns org.scode.sob
-  (:gen-class))
+  (:gen-class)
+  (:require [org.scode.sob.markdown :as markdown]
+            [clojure.contrib.command-line :as cmdline]
+            [clojure.contrib.logging :as logging]))
+
+(defn die [msg]
+  (logging/fatal msg)
+  (System/exit 1))
+
+(defn serve-blog
+  "Start serving a blog at http://*:port/base."
+  [port base path]
+  (logging/info (str "not impl serve " port " " base " " path)))
 
 (defn -main [& args]
-  (println "This is sob."))
+  (cmdline/with-command-line args
+    "sob - scode's own blog"
+    [[port     "Select listen port" 8081]
+     [base     "Select base URI path" "/"]
+     paths]
+    (if (> 1 (count paths))
+      (die "only one file system path currently supported"))
+    (if (empty? paths)
+      (die "must specify the path to a blog"))
+    (serve-blog port base (first paths))))
+
+
 
