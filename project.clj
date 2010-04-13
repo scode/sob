c385ada0c8d0acd97031390c33bf7b756d878339
Peter Schuller <peter.schuller@infidyne.com>
2010-04-13 23:34:37 +0200

    Depend on newer clojure/clojure-contrib, and depend on ring instead of compojure.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sob "0.0.1"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [rhino/js "1.7R2"]
-                 [compojure "0.3.2"]]
+                 [org.clojars.rnewman/ring "0.2.2-sessions"]]
   :main org.scode.sob)
