a12df10409c2837b96a0fe2abbd452baf34a358f
Peter Schuller <peter.schuller@infidyne.com>
2010-07-02 01:04:07 +0200

    Drop leiningen project def in favor of maven.

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject sob "0.0.1"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [rhino/js "1.7R2"]
-                 [org.clojars.rnewman/ring "0.2.2-sessions"]]
-  :main org.scode.sob)
